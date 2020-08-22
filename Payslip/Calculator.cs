25090a2657654a056e49f6811b5667cf6207bb2d
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-22 19:05:34 +1000

    Changed the structure of Payslip project

---
+++ Payslip/Calculator.cs
@@ -0,0 +1,46 @@
+using System;
+using userinput;
+using tool;
+using System.Collections.Generic;
+
+namespace calculator
+{
+    class Calculator
+    {
+        public static int calculateGrossIncome(Dictionary<string, string> userDataSet)
+        {
+          int annualSalary = int.Parse(userDataSet["annualSalary"]);
+          int grossIncome = annualSalary/12;
+          return grossIncome;
+        }
+        public static int calculateTax(Dictionary<string, string> userDataSet)
+        { double tax;
+          double annualSalary = double.Parse(userDataSet["annualSalary"]);
+          if(annualSalary<=18200){
+            tax = 0;
+          }else if(annualSalary>18200 && annualSalary<=37000){
+            tax = annualSalary * 0.19;
+          }else if(annualSalary>37000 && annualSalary<=87000){
+            tax = 3572 + (annualSalary - 37000)*0.325;
+          }else if(annualSalary>87000 && annualSalary<=180000){
+            tax = 19822 + (annualSalary - 87000)*0.37;
+          }else{
+            tax = 54232 + (annualSalary - 54232)*0.45;
+          }
+          double taxPerMonth = tax/12;
+          int incomeTax = Convert.ToInt32(Math.Ceiling(taxPerMonth));
+          return incomeTax;
+        }
+
+        public static int calculateNetIncome(int grossIncome, int incomeTax){
+          return (grossIncome - incomeTax);
+        }
+
+        public static int calculateSuper(Dictionary<string, string> userDataSet, int grossIncome){
+          double rate = double.Parse(userDataSet["superRate"])/100;
+          double super = grossIncome * rate;
+          int superInt = Convert.ToInt32(super);
+          return superInt;
+        }
+    }
+}
---
+++ Payslip/Tool.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic; 
+namespace tool
+{
+    public static class Tool
+    {
+        public static void SeparateCode()
+        {
+              Console.WriteLine("");
+        }
+        public static void greetUser(){
+            Console.WriteLine("Welcome to the payslip generator!");
+          }
+        public static void thankUser(){
+            Console.WriteLine("Thank you for using MYOB!");
+          }
+        public static void PrintDictionary(Dictionary<string, string> userDataSet)
+        {
+          foreach(KeyValuePair<string, string> keyValuePair in userDataSet)
+            {
+              Console.WriteLine("{0}: {1}", keyValuePair.Key, keyValuePair.Value);
+            }
+          }
+    }
+}
