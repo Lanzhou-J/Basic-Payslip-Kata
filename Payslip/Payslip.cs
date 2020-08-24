4d15763bb532d0de296744b69ae518cb6f4b7c0f
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-24 10:33:06 +1000

    changed class Payslip

--- Payslip/Payslip.cs
+++ Payslip/Payslip.cs
@@ -2,9 +2,26 @@
 // using System.Collections.Generic; 
 namespace payslip
 {
-  // public class Payslip:Employee
-  // {
-  //   public string paymentStartDate = "1 March";
-  //   public string paymentEndDate = "31 March";
-  // }
+  public class Payslip
+  {
+   public string PaymentStartDate
+   {get; set;}
+   public string PaymentEndDate
+   {get; set;}
+   public int GrossIncome
+   {get; set;}
+   public int IncomeTax
+   {get; set;}
+   public int NetIncome
+   {get; set;}
+   public int Super
+   {get; set;}
+
+   public Employee employee;
+   public Payslip(string paymentStartDate, string paymentEndDate, Employee employeeInput){
+    employee = employeeInput;
+    PaymentStartDate = paymentStartDate;
+    PaymentEndDate = paymentEndDate;
+   }
+  }
 }
